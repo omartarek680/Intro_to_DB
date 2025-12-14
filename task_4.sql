4b78106fed8a2a97a48638908e499e879e39fb81
Omar Tarek <omartarek680@gmail.com>
2025-12-14 23:28:12 +0200

    Task 4 Updates

--- task_4.sql
+++ task_4.sql
@@ -8,5 +8,5 @@ SELECT
     COLUMN_DEFAULT AS `Default`,
     EXTRA AS `Extra`
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()  -- Uses the database passed in the MySQL command
-  AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
